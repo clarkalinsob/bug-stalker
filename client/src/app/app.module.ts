16dd0c1b759bf04c8ef5b2a2b1dfbc7cf091510c
Clark Egbert Alinsob <clarkalinsob101@gmail.com>
2020-02-28 18:51:11 +0800

    added logo, navbar sticky-top

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,12 +10,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { EgAppCardComponent } from './eg-app-card/eg-app-card.component';
+import { FooterComponent } from './footer/footer.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    EgAppCardComponent
+    EgAppCardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
